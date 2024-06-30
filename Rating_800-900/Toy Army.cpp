32b03d1d76a5a62d66b334619f7968832492f35b
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-06-30 09:27:56 -0700

    contest: Codeforces Beta Round 72 (Div. 2 Only)_900_Rating

---
+++ Rating_800-900/Toy Army.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t = 1;
+    //cin>> t;
+    while(t--) {
+        int n;
+        cin>> n;
+        cout << (2*n)-(n/2) << endl;
+    }
+}
