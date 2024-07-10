3b1c6624c0f5b48fa035cbd3dbf43959462911d8
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-07-10 10:33:44 -0700

    contest: Codeforces Round 227 (Div. 2)_900_Rating

---
+++ Rating_800-900/George and Sleep.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d;
+    scanf("%d:%d%d:%d",&a,&b,&c,&d);
+    printf("%02d:%02d",(a-c-(d>b)+24)%24,(b-d+60)%60);
+    return 0;
+}
