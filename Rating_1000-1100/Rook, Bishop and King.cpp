f0e8f70421548a799362c2c76a27146a76c5aa05
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2024-09-19 12:55:45 +0600

    contest: Codeforces Round 217 (Div. 2)_1100_Rating

---
+++ Rating_1000-1100/Rook, Bishop and King.cpp
@@ -0,0 +1,101 @@
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  $                                             $
+  $   Author: marufhussai745                    $
+  $   Institute: JKKNIU                         $
+      Problem: A_Rook_Bishop_and_King                
+  $   Date: 2024/09/19                          $
+  $   Time: 12:28:47                            $
+  $                                             $
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+#include <queue>
+#include <climits>
+#include<stdio.h>
+
+using namespace std;
+
+#define pi 3.1415926535897932384626433832795028841971693993751058
+#define uni(x) unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n) upper_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define lb(x,n) lower_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define input(x) scanf("%d",&(x))
+#define int int64_t
+#define endl "\n"
+#define mod 1000000007
+#define chars "0abcdefghijklmnopqrstuvwxyz"
+
+string dtob(int n){ string m = ""; while(n!=0){ if(n%2==0) m='0'+m; else m='1'+m; n/=2; } return m;}
+
+int btod(string s) { int sz = s.size(),des = 0; reverse(s.begin(),s.end());
+    for(int i = 0; i<sz; i++) if(s[i] == '1') des += pow(2,i); return des; }
+
+int gcd(int a,int b){
+    int x = max({a,b}); int y = min({a,b}); int rem = x%y; while(rem!=0){
+    x = y; y = rem; rem = x%y;} return y; }
+
+int lcm(int x, int y){  return ((x*y)/gcd(x,y)); }
+
+int nPr(int n, int r){ int ans = 1; for(int i = n - r + 1 ; i <= n ; i++) ans*=i; return ans;}
+
+int nCr(int n, int r) { if (r > n) return 0; if (r == 0 || r == n) return 1;
+    return nCr(n - 1, r - 1) + nCr(n - 1, r); }
+
+bool prime_check(int n) { if(n<2) return false; else if(n <4) return true;
+    else if(n%2==0) return false; else { for(int i = 3; i<=sqrt(n); i+= 2) {
+    if(n%i==0) return false; } return true; }}
+
+bool int_palindrome(int n) { int int1 = n , int2 = 0; while( n!=0 ) {
+    int2 = int2*10 + (n%10); n /= 10; } if(int1 == int2 ) return true;
+    else return false;}
+
+bool array_Palindrome(const vector<int>& vec) { int start = 0; int end = vec.size() - 1;
+    while (start < end) { if (vec[start] != vec[end]) { return false; } start++; end--;} return true;}
+
+int factorial(int n) {int ans = 1; while(n>=1) {ans *= n; n--;} return ans;}
+
+int sum_of_digit(int n) { int sum = 0; while(n != 0) { sum += n % 10; n /= 10; } return sum;}
+
+int binary_search(vector<int> &A, int x) { int mn = 0, mx = A.size() - 1; int val = -1; while(mn <= mx) {
+    int mid = (mn + mx) / 2; if(A[mid] < x) mn = mid +1; else if(A[mid] == x) return mid;
+    else { mx = mid -1; } } return val;}
+
+int power(int A, int B) { if (B == 0) return 1; int res = power(A, B / 2); if (B % 2)
+    return (res * res * A) % mod; else return (res * res) % mod; }
+
+vector<int> primes(int n) { vector<int> prime(n+1, 0), ans; for(int i = 2; i*i <= n; i++) { if(prime[i] == 0) {
+    for(int j = 2; i*j <= n; j++) { prime[i*j] = 1; } } } for(int i = 2; i <= n; i++) { if(prime[i] == 0)
+    ans.push_back(i); } return ans; }
+
+bool triangle(int a, int b, int c) { if(a+b <= c || a+c <= b || b+c <= a) return false; else return true;}
+
+int t = 1, cs = 0;
+int32_t main() {
+    //ios::sync_with_stdio(0),cin.tie(0),cout.tie(0), cin >> t;
+    //cin >> t;
+    while(t--) {
+        int a, b, c, d;cin>> a>> b>> c>>d;
+        if(a == c || b == d) cout << 1 << " ";
+        else cout << 2 << " ";
+        if((a+b)%2 != (c+d)%2) cout << 0 << " ";
+        else {
+            map<pair<int,int>, int>mp;
+            int e=a,f=b;
+            while(e>0&&f>0) {mp[{e,f}]++; e--,f--;}
+            e=a,f=b;
+            while(e>0&&f<9) {mp[{e,f}]++; e--,f++;}
+            e=a,f=b;
+            while(e<9&&f>0) {mp[{e,f}]++; e++,f--;}
+            e=a,f=b;
+            while(e<9&&f<9) {mp[{e,f}]++; e++,f++;}
+            if(mp[{c,d}]>0) cout << 1 << " ";
+            else cout << 2 << " ";
+        }
+        cout << max({abs(a-c), abs(b-d)}) << endl;
+    }
+    return 0;
+}
