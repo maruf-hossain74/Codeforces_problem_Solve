746e04d8b874b8a3768a6a4fd1d52279f4aa4bb6
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-01-17 22:39:25 +0600

    Update Chess Placing.cpp

--- Chess Placing.cpp
+++ Chess Placing.cpp
@@ -4,7 +4,6 @@ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 $$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
 $$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
 $$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
-
 #include<bits/stdc++.h>
 #include<algorithm>
 #include<string.h>
