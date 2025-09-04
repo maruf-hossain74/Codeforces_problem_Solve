640cf6dd1fa8ef51622d00b4cb8c8a5b6edf13a6
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-09-04 20:19:27 +0600

    contest: Codeforces Beta Round 89 (Div. 2)_1000_Rating

---
+++ Rating_1000-1100/Present from Lena.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+    cin>>n;
+    for(int i=0; i<=n; i++)
+    {
+    	for(int j=1; j<=n-i; j++)
+        cout<<"  ";
+        for(int j=0; j<=i; j++)
+        {
+        	if(j==0)
+        		cout<<j;
+        	else
+        		cout<<" "<<j;
+        }
+        for(int j=i-1; j>=0; j--)
+        cout<<" "<<j;
+       cout<<"\n";
+    }
+    for(int i=n-1; i>=0; i--)
+    {
+    	for(int j=1; j<=n-i; j++)
+        cout<<"  ";
+        for(int j=0; j<=i; j++)
+        {
+        	if(j==0)
+        		cout<<j;
+        	else
+        		cout<<" "<<j;
+        }
+        for(int j=i-1; j>=0; j--)
+        cout<<" "<<j;
+       cout<<"\n";
+    }
+    return 0;
+}
