a965f1cb4839e926e86492d701a698eecdfaf820
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-09-12 19:25:24 +0600

    contest: Codeforces Round 367 (Div. 2)_1100_Rating

---
+++ Rating_1000-1100/Interesting drink.cpp
@@ -0,0 +1,70 @@
+/*########বিসমিল্লাহির রাহমানির রাহিম#####################
+##########################################
+#JATIYA KABI KAJI NAZRUL ISLAM UNIVERSITY#
+###########MD. MARUF HOSSAIN##############
+###########DEPARTMENT OF CSE##############
+###########SEASION: 2021-2022 ##########*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define ssortS  sort(S.begin(),S.end())
+#define ssortS1 sort(S1.begin(),S1.end())
+#define ssortS2 sort(S2.begin(),S2.end())
+#define asortA  sort(A,A+n)
+#define asortA1 sort(A1,A1+n)
+#define asortA2 sort(A2,A2+n)
+#define srevS   reverse(S.begin(),S.end())
+#define srevS1  reverse(S1.begin(),S1.end())
+#define srevS2  reverse(S2.begin(),S2.end())
+#define arevA   reverse(A,A+n)
+#define arevA1  reverse(A1,A1+n)
+#define arevA2  reverse(A2,A2+n)
+#define ll      long long
+#define ull     unsigned long long
+int gcd(int a, int b)
+{
+    int x=max({a,b});
+    int y=min({a,b});
+    int rem = y;
+    while(rem!=0)
+    {
+        rem=x%y;
+        x=y;
+        y = rem;
+    }
+    return x;
+}
+
+int main()
+{
+    ll n;
+    cin>>n;
+    vector<ll>M(n);
+    for (ll i=0; i<n; i++)
+        cin>>M[i];
+    sort(M.begin(),M.end());
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll x,ans;
+        cin>>x;
+        ans = upper_bound(M.begin(),M.end(),x)-M.begin();
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
