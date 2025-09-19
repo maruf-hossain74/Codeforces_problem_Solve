19a79f47d790021b6c9efbfa2e15ca90d67faa56
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-09-19 14:13:36 +0600

    contest: Codeforces Round 171 (Div. 2)_1400_Rating

---
+++ Rating_1400-1500/Books.cpp
@@ -0,0 +1,78 @@
+
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAJI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssortS  sort(S.begin(),S.end())
+#define asort(A,n)  sort((A),(A)+(n))
+#define srevS   reverse(S.begin(),S.end())
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin();
+#define arev(A,n)   reverse((A),(A)+(n))
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define vi      vector<int>
+#define vl      vector<long long>
+#define vc      vector<char>
+
+int gcd(int a, int b)
+{
+    int x=max({a,b});
+    int y=min({a,b});
+    int rem = y;
+    while(rem!=0)
+    {
+        rem=x%y;
+        x=y;
+        y = rem;
+    }
+    return x;
+}
+
+int main()
+{
+    ll n,m;
+    cin>>n>>m;
+    ll A[n];
+    ll sum=0,t=0,cnt=0;
+    for(ll i=0; i<n; i++)
+    {
+        cin>>A[i];
+        sum+=A[i];
+        if(sum>m)
+        {
+            if(cnt<i-t)
+            cnt = i-t;
+            sum-=A[t];
+            t++;
+        }
+        else if (i==n-1)
+        {
+            if(cnt<i-t+1)
+                cnt = i-t+1;
+        }
+    }
+    cout<<cnt<<"\n";
+    return 0;
+}
