82d6d8d0592b083ec3935d28a70fb566046fc73c
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-09-23 20:18:26 +0600

    contest: Codeforces Round 284 (Div. 2)_1000_Rating

---
+++ Rating_1000-1100/Lecture.cpp
@@ -0,0 +1,61 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAJI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssortS  sort(S.begin(),S.end())
+#define asortA  sort(A,A+n)
+#define srevS   reverse(S.begin(),S.end())
+#define arevA   reverse(A,A+n)
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define viA     vector<int>A
+#define vlA     vector<long long>A
+#define vcS     vector<char>S
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    map<string,string> mp;
+    for(int i = 0; i<m; i++)
+    {
+        string s,k;
+        cin>>s>>k;
+        if(s.size()<= k.size())
+            mp[s] = s;
+        else
+            mp[s] = k;
+    }
+    string ans[n];
+    for(int i = 0; i<n ; i++)
+    {
+        string st;
+         cin>>st;
+         ans[i] = mp[st];
+    }
+    for(int i = 0; i<n; i++)
+        cout<<ans[i]<<" ";
+    cout<<endl;
+    return 0;
+}
