338481d84fda1e2f7c864d1c9c65b6fc73af472b
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-09-23 20:27:03 +0600

    contest: Codeforces Round 273 (Div. 2)_1300_Rating

---
+++ Rating_1200-1300/Random Teams.cpp
@@ -0,0 +1,67 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAZI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssort(S)  sort((S).begin(),(S).end())
+#define asort(A)  sort((A),(A)+(n))
+#define srev(S)   reverse((S).begin(),(S).end())
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin();
+#define arev(A)   reverse((A),(A)+(n))
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define vi      vector<int>
+#define vl      vector<long long>
+#define vc      vector<char>
+
+int gcd(int a, int b)
+{
+    int x=max({a,b});
+    int y=min({a,b});
+    int rem = y;
+    while(rem!=0)
+    {
+        rem=x%y;
+        x=y;
+        y = rem;
+    }
+    return x;
+}
+
+int lcm(int x, int y)
+{ return ((x*y)/gcd(x,y)); }
+
+int main()
+{
+    ll n,k;
+    cin>>n>>k;
+    //low
+    ll AR1 = ((n/k)*((n/k)-1))/2;
+    ll AR2 = ((n/k)*((n/k)+1))/2;
+    ll low = ((k - (n%k)) * AR1) + ((n%k) * AR2);
+    //high
+    ll high = ((n-k+1) * (n - k)) / 2;
+    cout<<low << " " << high<<"\n";
+    return 0;
+}
