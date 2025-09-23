ac4dc977a48dd9bcf2ef019b03378f7520054121
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-09-23 20:22:56 +0600

    contest: Codeforces Round 720 (Div. 2)_1000_Rating

---
+++ Rating_1000-1100/Nastia and Nearly Good Numbers.cpp
@@ -0,0 +1,65 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAZI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssortS  sort(S.begin(),S.end())
+#define asortA  sort(A,A+n)
+#define srevS   reverse(S.begin(),S.end())
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin();
+#define arevA   reverse(A,A+n)
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define vi      vector<int>
+#define vl      vector<long long>
+#define vc      vector<char>
+
+int gcd(int a, int b)
+{
+    int x=max({a,b});
+    int y=min({a,b});
+    int rem = y;
+    while(rem!=0)
+    {
+        rem=x%y;
+        x=y;
+        y = rem;
+    }
+    return x;
+}
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        if(k==1) NO;
+        else 
+       cout<<"YES\n"<<n<<" "<<n*k<<" "<<n*(k+1)<<"\n";
+    }
+    return 0;
+}
