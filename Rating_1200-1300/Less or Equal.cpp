f5d9b4e03f7f7aa445ecb9a419501f77a9a3407b
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-10-10 23:54:10 +0600

    contest: Codeforces Round 479 (Div. 3)_1200_Rating

---
+++ Rating_1200-1300/Less or Equal.cpp
@@ -0,0 +1,82 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAZI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssortS  sort(S.begin(),S.end())
+#define asortA  sort(A,A+n)
+#define srevS   reverse(S.begin(),S.end())
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin();
+#define arevA   reverse(A,A+n)
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define vi      vector<int>
+#define vl      vector<long long>
+#define vc      vector<char>
+
+int gcd(int a, int b)
+{
+    int x=max({a,b});
+    int y=min({a,b});
+    int rem = y;
+    while(rem!=0)
+    {
+        rem=x%y;
+        x=y;
+        y = rem;
+    }
+    return x;
+}
+
+int lcm(int x, int y)
+{ return ((x*y)/gcd(x,y)); }
+
+int main()
+{
+        ll  n,k;
+        cin>>n>>k;
+        ll A[n];
+        map<ll,ll>mp;
+        for(ll i = 0 ; i<n ; i++)
+        {
+            cin>>A[i];
+            mp[A[i]]++;
+        }
+        ll sum=0,flag;
+        for(auto it: mp)
+        {
+            if(sum == 0) flag = it.first;
+            sum += it.second;
+            if(sum == k)
+            {
+                cout<<it.first<<"\n";
+                break;
+            }  
+        }
+        if(k ==0 && flag>1)
+        cout<<1<<"\n";
+        else if(sum!= k)
+        cout<<-1<<"\n";
+    return 0;
+}
