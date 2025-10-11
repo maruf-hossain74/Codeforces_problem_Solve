a61f5bb63baf58fa3cdb40fe55135cc85ba95996
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-10-11 15:26:43 +0600

    contest: Codeforces Round 254 (Div. 2)_1200_Rating

---
+++ Rating_1200-1300/DZY Loves Chessboard.cpp
@@ -0,0 +1,95 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAZI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssort(S)  sort((S).begin(),(S).end())
+#define asort(A)  sort((A),(A)+(n))
+#define srev(S)   reverse((S).begin(),(S).end())
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin();
+#define arev(A)   reverse((A),(A)+(n))
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define vi      vector<int>
+#define vl      vector<long long>
+#define vc      vector<char>
+
+int gcd(ll a, ll b){
+    ll x=max({a,b});
+    ll y=min({a,b});
+    ll rem = y;
+    while(rem!=0){
+        rem=x%y;
+    x=y; y = rem;
+    } return x;
+}
+string dtob(ll n){
+     string m = "";
+    while(n!=0){ if(n%2==0)
+    m='0'+m; else m='1'+m;
+    n/=2; } return m;}
+    ll btod(string s){
+    ll sz = s.size(),des = 0;
+    reverse(s.begin(),s.end());
+    for(ll i = 0; i<sz; i++)
+        if(s[i] == '1') des += pow(2,i);
+    return des;
+}
+int lcm(ll x, ll y)
+{ return ((x*y)/gcd(x,y)); }
+
+int npr(int n, int r)
+{
+    ll  ans = 1;
+    for(int i = n - r + 1 ; i <= n ; i++)
+        ans*=i;
+    return ans;
+}
+int fact(int n)
+{
+    ll ans = 1;
+    for(int i = 2; i <= n; i++)
+        ans *= i;
+    return ans;
+}
+
+int main()
+{
+	// your code goes here
+	int n,m; cin>>n>>m;
+	char c;
+	string s[n];
+	for(int i = 0 ; i < n; i++)
+    {
+        for(int j = 0 ; j < m; j++)
+        {
+            cin>>c;
+            if(c == '.' && (i+j)%2==0) s[i] += "B";
+            if(c == '.' && (i+j)%2==1) s[i] += "W";
+            if(c == '-') s[i] += "-";
+        }
+    }
+    for(int i = 0 ; i < n; i++) cout<<s[i]<<'\n';
+	return 0;
+}
