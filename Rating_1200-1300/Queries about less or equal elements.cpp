90718ba31c7e09f3135c19c4d9d1c3dba6f2a014
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-10-11 15:32:09 +0600

    contest: Educational Codeforces Round 2_1300_Rating

---
+++ Rating_1200-1300/Queries about less or equal elements.cpp
@@ -0,0 +1,96 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAZI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssort(S)  sort((S).begin(),(S).end())
+#define asort(A)  sort((A),(A)+(n))
+#define srev(S)   reverse((S).begin(),(S).end())
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define arev(A)   reverse((A),(A)+(n))
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define vi      vector<int>
+#define vl      vector<long long>
+#define vc      vector<char>
+
+int gcd(ll a, ll b){
+    ll x=max({a,b});
+    ll y=min({a,b});
+    ll rem = y;
+    while(rem!=0){
+        rem=x%y;
+    x=y; y = rem;
+    } return x;
+}
+string dtob(ll n){
+     string m = "";
+    while(n!=0){ if(n%2==0)
+    m='0'+m; else m='1'+m;
+    n/=2; } return m;}
+    ll btod(string s){
+    ll sz = s.size(),des = 0;
+    reverse(s.begin(),s.end());
+    for(ll i = 0; i<sz; i++)
+        if(s[i] == '1') des += pow(2,i);
+    return des;
+}
+int lcm(ll x, ll y)
+{ return ((x*y)/gcd(x,y)); }
+
+int npr(int n, int r)
+{
+    ll  ans = 1;
+    for(int i = n - r + 1 ; i <= n ; i++)
+        ans*=i;
+    return ans;
+}
+int fact(int n)
+{
+    ll ans = 1;
+    for(int i = 2; i <= n; i++)
+        ans *= i;
+    return ans;
+}
+
+int main()
+{
+	// your code goes here
+    int n,k;
+    cin>>n>>k;
+    vector<int> A;
+    int B[k],a;
+    for(int i = 0 ; i < n; i++)
+        {
+            cin>>a;
+            A.pb(a);
+        }
+    for(int i = 0 ; i< k; i++)
+        cin>>B[i];
+    sort(A.begin(),A.end());
+    for(int i = 0 ; i< k; i++)
+        cout<<ub(A,B[i])<<" ";
+        cout<<endl;
+	return 0;
+}
