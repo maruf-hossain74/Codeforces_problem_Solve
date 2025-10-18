50711f2567b941cd77aa0de9d187a8dbc20a79c6
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-10-18 20:01:58 +0600

    contest: Codeforces Round 605 (Div. 3)_1200_Rating

---
+++ Rating_1200-1300/Yet Another Broken Keyboard.cpp
@@ -0,0 +1,135 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAZI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define pi      3.1415926535897932384626433832795028841971693993751058
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssort(S)  sort((S).begin(),(S).end())
+#define asort(A)  sort((A),(A)+(n))
+#define srev(S)   reverse((S).begin(),(S).end())
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define lb(x,n)    lower_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define arev(A)   reverse((A),(A)+(n))
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define vi      vector<int>
+#define vl      vector<long long>
+#define vc      vector<char>
+#define input(x) scanf("%d",&(x))
+#define int int64_t
+#define speed (ios_base:: sync_with_stdio(false),cin.tie(NULL))
+
+string dtob(int n){
+     string m = "";
+    while(n!=0){ if(n%2==0)
+    m='0'+m; else m='1'+m;
+    n/=2; } return m;}
+int btod(string s){
+    int sz = s.size(),des = 0;
+    reverse(s.begin(),s.end());
+    for(int i = 0; i<sz; i++)
+        if(s[i] == '1') des += pow(2,i);
+    return des;
+}
+
+int gcd(int a,int b){
+    int  x = max({a,b});
+    int  y = min({a,b});
+    int rem = x%y;
+    while(rem!=0){
+    x = y; y = rem;
+    rem = x%y;}
+    return y;
+}
+
+int lcm(int x, int y)
+{ return ((x*y)/gcd(x,y)); }
+
+int npr(int n, int r)
+{
+    int ans = 1;
+    for(int i = n - r + 1 ; i <= n ; i++)
+        ans*=i;
+    return ans;
+}
+int nCr(int n, int r)
+{
+    if (r > n)
+        return 0;
+    if (r == 0 || r == n)
+        return 1;
+    return nCr(n - 1, r - 1) + nCr(n - 1, r);
+}
+
+
+
+bool prime_check(int n)
+{
+    if(n<2) return false;
+    else if(n <4) return true;
+    else if(n%2==0) return false;
+    else
+    {
+        for(int i = 3; i<=sqrt(n); i+= 2)
+        {
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
+
+int t = 1;
+int32_t main()
+{
+	//ios::sync_with_stdio(0),cin.tie(0),cout.tie(0),cin>>t;
+	//cin>>t;
+	while(t--)
+    {
+        int n,k; cin>>n>>k;
+        string s,sub; cin>>s;
+        char x;
+        for(int i = 0 ; i < k; i++)
+        {
+            cin>>x;
+            sub += x;
+        }
+        int cnt = 0, ans = 0 ;
+        for(int i = 0 ; i < n; i++)
+        {
+            int m = sub.find(s[i]);
+            if(m>=0 && m < k) cnt++;
+            else
+            {
+                ans += (cnt * (cnt+1))/2;
+                cnt=0;
+            }
+            if(i == n-1 && cnt!= 0)
+                ans += (cnt * (cnt+1))/2;
+
+        }
+        cout<<ans<<"\n";
+    }
+	return 0;
+}
+
