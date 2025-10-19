c6542e7a9fa29ea5dc4a265dcdef021540b8d777
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-10-20 00:32:50 +0600

    contest: Codeforces Round 725 (Div. 3)_1300_Rating

---
+++ Rating_1200-1300/Number of Pairs.cpp
@@ -0,0 +1,101 @@
+/*$$$$$$$$ বিসমিল্লাহির রাহমানির রাহিম $$$$$$$
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ JATIYA KABI KAZI NAZRUL ISLAM UNIVERSITY
+$$$$$$$$$ MD. MARUF HOSSAIN $$$$$$$$$$$$$$
+$$$$$$$$$ DEPARTMENT OF CSE $$$$$$$$$$$$$$
+$$$$$$$$$ SEASION: 2021-2022 $$$$$$$$$$$*/
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+#define YES     cout<<"YES"<<"\n"
+#define Yes     cout<<"Yes"<<"\n"
+#define yes     cout<<"yes"<<"\n"
+#define NO      cout<<"NO"<<"\n"
+#define No      cout<<"No"<<"\n"
+#define no      cout<<"no"<<"\n"
+#define m1      cout<<-1<<"\n"
+#define ssort(S)  sort((S).begin(),(S).end())
+#define asort(A)  sort((A),(A)+(n))
+#define srev(S)   reverse((S).begin(),(S).end())
+#define uni(x)    unique((x).begin(),(x).begin()+(x).size())-(x).begin()
+#define ub(x,n)    upper_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define lb(x,n)    lower_bound((x).begin(),(x).end(),(n))-(x).begin()
+#define arev(A)   reverse((A),(A)+(n))
+#define ll      long long
+#define ull     unsigned long long
+#define pb      push_back
+#define vi      vector<int>
+#define vl      vector<long long>
+#define vc      vector<char>
+#define input(x) scanf("%d",&(x))
+
+int gcd(ll a, ll b){
+    ll x=max({a,b});
+    ll y=min({a,b});
+    ll rem = y;
+    while(rem!=0){
+        rem=x%y;
+    x=y; y = rem;
+    } return x;
+}
+string dtob(ll n){
+     string m = "";
+    while(n!=0){ if(n%2==0)
+    m='0'+m; else m='1'+m;
+    n/=2; } return m;}
+    ll btod(string s){
+    ll sz = s.size(),des = 0;
+    reverse(s.begin(),s.end());
+    for(ll i = 0; i<sz; i++)
+        if(s[i] == '1') des += pow(2,i);
+    return des;
+}
+int lcm(ll x, ll y)
+{ return ((x*y)/gcd(x,y)); }
+
+int npr(int n, int r)
+{
+    ll  ans = 1;
+    for(int i = n - r + 1 ; i <= n ; i++)
+        ans*=i;
+    return ans;
+}
+int nCr(int n, int r)
+{
+    if (r > n)
+        return 0;
+    if (r == 0 || r == n)
+        return 1;
+    return nCr(n - 1, r - 1) + nCr(n - 1, r);
+}
+
+int main()
+{
+    ll t; cin>>t; while(t--)
+	{
+	    ll n,l,r,a; cin>>n>>l>>r;
+	    vector<ll>A(n);
+	    for(auto &a: A)
+            cin>>a;
+        sort(A.begin(),A.end());
+        ll ans = 0;
+        for(int i = 0 ; i < n; i++)
+        {
+             ans += ub(A,r-A[i]);
+             ans -= lb(A,l-A[i]);
+
+             if(l <=  2*A[i] && r >= 2*A[i]) ans--;
+             //cout<<ans <<" "<< ub(A,r-A[i])<< " "<<lb(A,l-A[i])<<endl;
+        }
+        cout<<ans/2<<"\n";
+	}
+  return 0;
+}
