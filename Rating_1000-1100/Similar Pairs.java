5c45e56c67c191c617e017631fce21862feacd0d
MD.Maruf Hossain <148317594+maruf-hossain74@users.noreply.github.com>
2025-10-22 12:11:55 +0600

    contest: Codeforces Round 644 (Div. 3)_1100_Rating

---
+++ Rating_1000-1100/Similar Pairs.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        input.nextLine();
+        while(t-- > 0) {
+            int n = input.nextInt();
+            int A[] = new int[n];
+            int even = 0, odd = 0, same = 0;
+            for(int i = 0; i < n; i++) {
+                A[i] = input.nextInt();
+                if(A[i] % 2 == 0) {
+                    even++;
+                }
+                else {
+                    odd++;
+                }
+            }
+            if(even > odd) same = even - odd;
+            else same = odd - even;
+            if(even % 2 == 0) {
+                System.out.println("YES");
+            }
+            else {
+                Arrays.sort(A);
+                boolean flag = false;
+                for(int i = 1; i < n; i++) {
+                    if(A[i] == A[i - 1] + 1) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag) {
+                    System.out.println("YES");
+                }
+                else {
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+}
